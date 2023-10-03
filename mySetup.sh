7dfa1d8a44231d31754d5673ec09d7b0376a1796
loganparanto <145365287+loganparanto@users.noreply.github.com>
2023-10-03 17:36:03 -0400

    Update mySetup.sh

--- mySetup.sh
+++ mySetup.sh
@@ -3,27 +3,24 @@
 # Usage: ./mySetup.sh
 # Example: ./mySetup.sh
 
-# Task a: Set the frequency of the BBB to 600 MHz.
+# Task a:
 echo "Setting the frequency of BBB to 600 MHz."
-# Replace the following command with the actual command to set the frequency.
-# Example: sudo cpufreq-set -c 0 -f 600MHz
-# Make sure you have the necessary permissions to set the CPU frequency.
 
-# Task b: Display information on the CPU using cpufreq-info command.
+# Task b:
 echo "Displaying CPU information:"
 cpufreq-info
 
-# Task c: Create a variable named location and assign it the value: /home/debian
+# Task c:
 location="/home/debian"
 
-# Task d: Echo the value of the variable location.
+# Task d:
 echo "Location is set to: $location"
 
-# Task e: Create a variable named items and display the number of items in the current working directory.
+# Task e:
 items=$(ls | wc -l)
 echo "Number of items in the current directory: $items"
 
-# Task 2: Modify the PATH environment variable to execute the script from any directory.
+# Task 2:
 # Add the script's directory to the PATH variable
 script_directory="$(cd "$(dirname "$0")" && pwd)"
 export PATH="$PATH:$script_directory"
